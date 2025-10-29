a4707a30798c083e97625128bcd1c8682d5d3048
astroPrince1031 <astroPrince1031@gmail.com>
2025-10-29 22:53:38 +0900

    joinLogic

--- miniProject/TestSecurity/src/main/java/com/example/TestSecurity/service/JoinService.java
+++ miniProject/TestSecurity/src/main/java/com/example/TestSecurity/service/JoinService.java
@@ -1,12 +1,32 @@
 package com.example.TestSecurity.service;
 
 import com.example.TestSecurity.dto.JoinDTO;
+import com.example.TestSecurity.entity.UserEntity;
+import com.example.TestSecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class JoinService {
 
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public void joinProcess(JoinDTO joinDTO) {
+
+        // DB에 해당 USER가 이미 있는지?
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(joinDTO.getUsername());
+        data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+        data.setRole("ROLE_USER");
+
+        userRepository.save(data);
 
 
     }
